6a76ee31a8647af66428b45b4f42173408d3a697
clucien <chastanlucien@gmail.com>
2023-09-04 17:59:15 +0200

    Démarrage du thread à l'initialisation de RorVsWild

--- config/initializers/rorvswild.rb
+++ config/initializers/rorvswild.rb
@@ -1,3 +1,6 @@
 # frozen_string_literal: true
 
-RorVsWild.start(api_key: ENV['ROR_WS_WILD_API_KEY'], features: ['server_metrics']) if ENV.fetch('ENABLE_ROR_WS_WILD', false) == 'true'
+if ENV.fetch('ENABLE_ROR_WS_WILD', false) == 'true'
+  RorVsWild.start api_key: ENV['ROR_WS_WILD_API_KEY']
+  RorVsWild.agent.queue.start_thread
+end

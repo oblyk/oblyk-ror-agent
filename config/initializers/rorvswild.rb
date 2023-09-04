fb1d29965049cbe988464a6013bdedeb131ae391
clucien <chastanlucien@gmail.com>
2023-09-04 18:16:38 +0200

    Ajout de l'update de la queue plus ligne de fin de fichier

--- config/initializers/rorvswild.rb
+++ config/initializers/rorvswild.rb
@@ -2,5 +2,6 @@
 
 if ENV.fetch('ENABLE_ROR_WS_WILD', false) == 'true'
   RorVsWild.start api_key: ENV['ROR_WS_WILD_API_KEY']
+  RorVsWild.agent.queue.instance_variable_get(:@metrics).update
   RorVsWild.agent.queue.start_thread
 end

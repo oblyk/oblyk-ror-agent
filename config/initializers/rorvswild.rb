46dfeb49c362b813b4efff535af4a4b409779bdb
clucien <chastanlucien@gmail.com>
2023-09-04 18:17:11 +0200

    Suppression de la condition d'activation de RoRVsWild

--- config/initializers/rorvswild.rb
+++ config/initializers/rorvswild.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-if ENV.fetch('ENABLE_ROR_WS_WILD', false) == 'true'
-  RorVsWild.start api_key: ENV['ROR_WS_WILD_API_KEY']
-  RorVsWild.agent.queue.instance_variable_get(:@metrics).update
-  RorVsWild.agent.queue.start_thread
-end
+RorVsWild.start api_key: ENV['ROR_WS_WILD_API_KEY']
+RorVsWild.agent.queue.instance_variable_get(:@metrics).update
+RorVsWild.agent.queue.start_thread

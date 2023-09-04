c3f373ef8344d1b76cdd84d8673577f596ed4280
clucien <chastanlucien@gmail.com>
2023-09-04 18:23:43 +0200

    Suppression de la partie update

--- config/initializers/rorvswild.rb
+++ config/initializers/rorvswild.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
-RorVsWild.start api_key: ENV['ROR_WS_WILD_API_KEY']
-RorVsWild.agent.queue.instance_variable_get(:@metrics).update
+RorVsWild.start(api_key: ENV['ROR_WS_WILD_API_KEY'])
 RorVsWild.agent.queue.start_thread
